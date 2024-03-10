37f5eff95669f4889f711907e69c36f469c79866
Diivyak <divyani.02@gmail.com>
2024-03-10 19:07:20 +0530

    intial commit

---
+++ src/test/java/com/scaler/myscalerproject/MyScalerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.myscalerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScalerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
