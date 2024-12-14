6c89db061636a37faea06bc1c61489e923c77f7b
Diivyak <divyani.02@gmail.com>
2024-12-14 16:47:22 +0530

    queries

--- src/main/java/com/scaler/myscalerproject/repositories/ProductRespository.java
+++ src/main/java/com/scaler/myscalerproject/repositories/ProductRespository.java
@@ -4,6 +4,12 @@ import com.scaler.myscalerproject.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRespository extends JpaRepository<Product, Long> {
+    List<Product> findByTitleContaining(String word);
+
+    long deleteByTitle(String title);
+
 }
--- src/test/java/com/scaler/myscalerproject/MyScalerProjectApplicationTests.java
+++ src/test/java/com/scaler/myscalerproject/MyScalerProjectApplicationTests.java
@@ -1,13 +1,27 @@
 package com.scaler.myscalerproject;
 
+import com.scaler.myscalerproject.repositories.ProductRespository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
 @SpringBootTest
 class MyScalerProjectApplicationTests {
 
+	@Autowired
+	public ProductRespository productRespository;
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	@Transactional
+	@Commit
+	void testQueries() {
+		productRespository.findByTitleContaining("divya");
+		productRespository.deleteByTitle("divya");
+
 	}
 
 }
